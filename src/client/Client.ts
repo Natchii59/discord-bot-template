9bb02e90e64b7cbc6bb8d7912cc64bcb021638f3
Nathan Caron <caron.nathan@hotmail.com>
2022-07-08 22:32:54 +0200

    Create Base of Client
    Add User entity

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,13 +1,46 @@
-import { Client } from 'discord.js';
+import { Client, Intents } from 'discord.js';
+import { ActivityTypes } from 'discord.js/typings/enums';
+import { join } from 'path';
+import { createConnection, getRepository, Repository } from 'typeorm';
+
+import { User } from '../entities/user.entity';
+import Handler from './handler/Handler';
 
 export default class MyClient extends Client {
+  public handler: Handler = new Handler(this);
+
+  public userRepository: Repository<User>;
+
   constructor() {
     super({
-      intents: [],
+      intents: [Intents.FLAGS.GUILDS],
+      presence: {
+        activities: [
+          {
+            name: 'his computer',
+            type: ActivityTypes.WATCHING,
+          },
+        ],
+      },
     });
   }
 
-  async start(): Promise<void> {
+  public async start(): Promise<void> {
+    await createConnection({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [join(__dirname, '..', '**', '*.entity.{js,ts}')],
+      synchronize: true,
+    });
+
+    this.userRepository = getRepository(User);
+
+    await this.handler.start();
+
     await this.login(process.env.DISCORD_BOT_TOKEN);
   }
 }
